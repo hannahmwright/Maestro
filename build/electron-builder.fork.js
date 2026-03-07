66acfb9e1735f0d79097b40fd82c883573fdd6c8
Hannah Wright <hannahbwright@Mac.attlocal.net>
2026-03-07 18:50:38 -0500

    Fix fork release packaging config

---
+++ build/electron-builder.fork.js
@@ -0,0 +1,34 @@
+/* global require, module */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+const packageJson = require('../package.json');
+
+module.exports = {
+	...packageJson.build,
+	appId: 'com.vervetechgroup.maestro.fork',
+	productName: 'Maestro Fork',
+	publish: {
+		provider: 'github',
+		owner: 'hannahmwright',
+		repo: 'Maestro',
+	},
+	extraMetadata: {
+		repository: {
+			type: 'git',
+			url: 'https://github.com/hannahmwright/Maestro.git',
+		},
+		maestroRelease: {
+			current: {
+				label: 'Hannah Fork Releases',
+				owner: 'hannahmwright',
+				repo: 'Maestro',
+			},
+			upstream: {
+				label: 'RunMaestro Releases',
+				owner: 'RunMaestro',
+				repo: 'Maestro',
+			},
+			fork: packageJson.maestroRelease?.fork,
+		},
+	},
+};
